842cce030223870dc7e3d4d28ea3cf3a5bcd0d48
Peter Kaisinger <Peter.kaisinger@faktorzehn.at>
2018-05-18 12:05:24 +0200

    Update Styling

--- frontend/src/app/text-generator.service.ts
+++ frontend/src/app/text-generator.service.ts
@@ -9,16 +9,12 @@ import 'rxjs/add/operator/map';
 @Injectable()
 export class TextGeneratorService {
 
-  private static readonly OFFLINE_PATH: string = '../assets/dummyResponse.json';
-  private static readonly ONLINE_PATH: string = 'http://localhost:1234/generate';
-  private static readonly API_PROXY_PATH: string = '/api/generate'
+  private static readonly API_PROXY_PATH: string = '/api/generate';
 
   constructor(private _httpClient: HttpClient) { }
 
   loadText(request: RequestModel): Observable<ResponseModel> {
     // need to use post request because backend can only load body payload for post requests
-
-    const headers = new HttpHeaders()
 
     return this._httpClient.post(
       TextGeneratorService.API_PROXY_PATH,
